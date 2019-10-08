13880d03a09d289d3a99f4fea72f6bb672214eae
rapdev <ubuntu@ip-172-31-17-148.ec2.internal>
2019-10-08 23:59:12 +0000

    'master' branch updated from  https://rapdeviodemo1.service-now.com no-cicd

--- sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.Demo 1.js
+++ sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.Demo 1.js
@@ -3,8 +3,10 @@
  * ClassName   : sys_script_include
  * Created On  : 2019-10-08 23:05:10
  * Created By  : shar
- * Updated On  : 2019-10-08 23:05:10
+ * Updated On  : 2019-10-08 23:57:55
  * Updated By  : shar
  * URL         : /sys_script_include.do?sys_id=977f66bedbdc0c50c7e55886dc9619c5
  */
 //Initialize
+//Update 1
+//Update 2 - after conflict
