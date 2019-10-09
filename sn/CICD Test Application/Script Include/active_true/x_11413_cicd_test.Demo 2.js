8d669211872331c5f784bfa02acdca0db8feade5
rapdev <ubuntu@ip-172-31-17-148.ec2.internal>
2019-10-09 00:32:56 +0000

    'master' branch updated from  https://rapdeviodemo1.service-now.com no-cicd

--- sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.Demo 2.js
+++ sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.Demo 2.js
@@ -3,8 +3,9 @@
  * ClassName   : sys_script_include
  * Created On  : 2019-10-09 00:22:46
  * Created By  : shar
- * Updated On  : 2019-10-09 00:22:46
+ * Updated On  : 2019-10-09 00:32:41
  * Updated By  : shar
  * URL         : /sys_script_include.do?sys_id=526187bedb104c50c7e55886dc961973
  */
 //Initialize Demo 2
+//Update 2 - Overriding conflict
