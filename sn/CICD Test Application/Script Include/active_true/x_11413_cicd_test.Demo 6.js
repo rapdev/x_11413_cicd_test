2b27154f977d4fd1cb176990de0f1836d14523b0
rapdev <ubuntu@ip-172-31-17-148.ec2.internal>
2020-01-30 17:18:06 +0000

    'master' branch updated from  https://rapdeviodemo1.service-now.com no-cicd

---
+++ sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.Demo 6.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : CICD Test Application
+ * ClassName   : sys_script_include
+ * Created On  : 2020-01-30 17:16:30
+ * Created By  : shar
+ * Updated On  : 2020-01-30 17:16:30
+ * Updated By  : shar
+ * URL         : /sys_script_include.do?sys_id=e2152c6bdb6a0c90ef7d52c8dc96191b
+ */
+//Demo 6
