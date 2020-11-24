2e97cb546421db3697848399aa30fc64a092ee29
rapdev <ubuntu@ip-172-31-17-148.ec2.internal>
2020-11-24 22:52:27 +0000

    'master' branch updated from  https://rapdeviodemo1.service-now.com no-cicd

--- sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.Demo 10.js
+++ sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.Demo 10.js
@@ -3,8 +3,9 @@
  * ClassName   : sys_script_include
  * Created On  : 2020-11-24 22:48:55
  * Created By  : shar
- * Updated On  : 2020-11-24 22:48:55
+ * Updated On  : 2020-11-24 22:51:59
  * Updated By  : shar
  * URL         : /sys_script_include.do?sys_id=189461ff1be82c508bc6a68a234bcb61
  */
 //Demo 10
+//is this going to require interaction?
