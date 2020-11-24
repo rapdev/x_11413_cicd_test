088b9c2009931dd802f3459ea69c2d5f2b604790
rapdev <ubuntu@ip-172-31-17-148.ec2.internal>
2020-11-24 22:44:44 +0000

    'master' branch updated from  https://rapdeviodemo1.service-now.com no-cicd

---
+++ sn/CICD Test Application/Business Rule/{--none--}/active_true/before/Testing BR22/script.js
@@ -0,0 +1,14 @@
+/* 
+ * Application : CICD Test Application
+ * ClassName   : sys_script
+ * Created On  : 2020-11-24 22:43:48
+ * Created By  : shar
+ * Updated On  : 2020-11-24 22:44:14
+ * Updated By  : shar
+ * URL         : /sys_script.do?sys_id=c563a57f1be82c508bc6a68a234bcb70
+ */
+(function executeRule(current, previous /*null when async*/) {
+
+	// Add your code here
+
+})(current, previous);
